d57b35ecc51e808ea981a191af2bdc751810f1eb
Chayden <chayden.richardson@gmail.com>
2023-01-13 16:31:40 -0800

    update tab header and icon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
+// !!!! npm run deploy !!!!
 import signature from './signature.gif';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className='header'>
-      <div className="sword-animation">
-        <div className='sword'> </div>
+    <div className='header'>
+      <div className='sword-animation'>
+        <div className='sword' />
       </div>
       <img src={signature} />
-
-    </div>
     </div>
   );
 }
